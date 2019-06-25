acabf1930791b21db302fed2229518b9657b6363
Raman Aujla <ramanpreet.aujla@gmail.com>
2019-06-25 12:54:42 -0400

    Rearranged functions in scrip.js file

--- js/script.js
+++ js/script.js
@@ -1,44 +1,9 @@
+// adds the cuurent-page class to the index number passed in the 
 function addCurrentClass(indexNum) {
     $('.mobile-menu li').removeClass('current-page');
     $('.mobile-menu li').eq(indexNum).closest('li').addClass('current-page');
     $('.desktop-menu li').removeClass('current-page');
     $('.desktop-menu li').eq(indexNum).closest('li').addClass('current-page');
-}
-
-// This function will handle all the scroll effects
-// 1 - Menu drops down when the user scrolls back up 
-// 2 - Smooth scroll to the target when user clicks a link
-function handleScrollEffects() {
-
-    // selects all links that have an href which begin with a '#'
-    let allLinks = $("a[href^='#']");
-
-    // add smooth scrolling to clicks on all the links
-    allLinks.click(function (e) {
-        e.preventDefault();
-        $('body, html').animate({
-            scrollTop: $(this.hash).offset().top - 40
-        }, 750);
-    })
-
-    // changes the active page selected in the menu on scrolling
-    let activeLinks = $('.menu-item a')
-    // Swithcing Active Links on Scrolling
-    $(window).scroll(function (e) {
-        let scrollbarLocation = $(this).scrollTop();
-        activeLinks.each(function () {
-
-            let sectionOffset = $(this.hash).offset().top - 125;
-            if($(window).scrollTop() + $(window).height() == $(document).height()) {
-                addCurrentClass(3);
-            }
-            if (sectionOffset <= scrollbarLocation) {
-                $(this).parent().addClass('current-page');
-                $(this).parent().siblings().removeClass('current-page');
-            }
-        });
-    });
-
 }
 
 // handles all the clicks on the menu buttons
@@ -67,7 +32,7 @@ function handleTogglingMobileMenu() {
     $('.menu-button').on("click", function (event) {
         $('.menu-button').toggleClass("change");
         if ($('.mobile-menu').css("display") === "none") {
-            $('.mobile-menu').css('display', 'inline-block');
+            $('.mobile-menu').css('display', 'block');
             $('.mobile-menu').slideDown(200);
 
         } else {
@@ -76,6 +41,53 @@ function handleTogglingMobileMenu() {
     });
 }
 
+//This function handles the functionality for the 
+//menu button on the mobile page
+function handleMenuButtonClicks() {
+    handleTogglingMobileMenu();
+    handleMenuItemClick();
+}
+
+// This function will handle all the scroll effects
+// 1 - Menu drops down when the user scrolls back up 
+// 2 - Smooth scroll to the target when user clicks a link
+function handleScrollEffects() {
+
+    // selects all links that have an href which begin with a '#'
+    let allLinks = $("a[href^='#']");
+
+    // add smooth scrolling to clicks on all the links
+    allLinks.click(function (e) {
+        e.preventDefault();
+        $('body, html').animate({
+            scrollTop: $(this.hash).offset().top - 40
+        }, 750);
+    })
+
+    // changes the active page selected in the menu on scrolling
+    let activeLinks = $('.menu-item a')
+    // Swithcing Active Links on Scrolling
+    $(window).scroll(function (e) {
+        let scrollbarLocation = $(this).scrollTop();
+        activeLinks.each(function () {
+
+            let sectionOffset = $(this.hash).offset().top - 125;
+            if($(window).scrollTop() + $(window).height() == $(document).height()) {
+                // gets the index for the last menu item so it can be 
+                // set as current menu item when bottom of web page is reached
+                let index = $(this).closest('li:last-child').index();
+                addCurrentClass(index);
+            }
+            if (sectionOffset <= scrollbarLocation) {
+                $(this).parent().addClass('current-page');
+                $(this).parent().siblings().removeClass('current-page');
+            }
+        });
+    });
+
+}
+
+// changes the class when the sizes are changed
 function handleMenuStyleOnResizing() {
     // Hide Mobile Menu Items when the page size increases
     $(window).on('resize', function (e) {
@@ -83,6 +95,12 @@ function handleMenuStyleOnResizing() {
         var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
         if (w > 989) {
             $('.menu-button').removeClass("change");
+
+            // BELOW FUNCTIONALITY: 
+            // remove class for mobile menu and add the class for desktop menu
+            // .show is to make it visible which results in adding a display: block, 
+            // because of which the .css (display, flex) had to be added to avoid that
+
             $('.menu-list-wrapper ul').removeClass('mobile-menu');
             $('.menu-list-wrapper ul').addClass('desktop-menu');
             $('.menu-list-wrapper ul').show();
@@ -91,12 +109,12 @@ function handleMenuStyleOnResizing() {
         }else{
             $('.menu-list-wrapper ul').addClass('mobile-menu');
             $('.menu-list-wrapper ul').removeClass('desktop-menu');
-            $('.mobile-menu').hide();
+            $('.mobile-menu').slideUp(0);
         }
     });
 }
 
-
+// add the class depending on the size of the page when it is loaded for responsive style
 function handleMenuStyleOnLoad(){
     var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
     if (w > 989) {
@@ -108,21 +126,11 @@ function handleMenuStyleOnLoad(){
     }
 }
 
-//This function handles the functionality for the 
-//menu button on the mobile page
-function handleMenuButtonClicks() {
-
-    // Displays the links to the menu items
-    handleTogglingMobileMenu();
-    handleMenuItemClick();
-    handleMenuStyleOnResizing();
-
-}
-
 // This function operates as the callback for all 
 // the functions required for the page
 function handlePageFunctions() {
     handleMenuStyleOnLoad();
+    handleMenuStyleOnResizing();
     handleScrollEffects();
     handleMenuButtonClicks();
 }
